c7c7274d160b87a0ef34ae7bd2a61ddec26b69a5
Celine-grow <celineloise4@gmail.com>
2024-03-16 14:11:16 +0300

    Dart Hackathon Programs

--- controlflow.dart
+++ controlflow.dart
@@ -1,7 +1,23 @@
 
-## Program 3: Determine Grade Based on Marks
-Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
-- If the marks are greater than 85, print "Excellent".
-- If the marks are between 75 and 85 (inclusive), print "Very Good".
-- If the marks are between 65 and 75 (inclusive), print "Good".
-- If the marks are below 65, print "Average".
+// Program 3: Determine Grade Based on Marks
+//Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
+//If the marks are greater than 85, print "Excellent".
+// If the marks are between 75 and 85 (inclusive), print "Very Good".
+// If the marks are between 65 and 75 (inclusive), print "Good".
+// If the marks are below 65, print "Average".
+import 'dart:io';
+void main(){
+  //getting student marks
+  print("Enter your marks:");
+  double?  marks=double.parse(stdin.readLineSync()!);
+//use of conditional if else statement to assign grade
+  if(marks>85){
+    print(("Excellent"));
+  }else if(marks>75){
+    print(("Very Good"));
+  }else if (marks>=65){
+    print(("Good"));
+  }else{
+    print(("Average"));
+  }
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,21 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+//## Program 1: Display Personal Information
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+import 'dart:io';
+void main(){
+  //asking for user input at run time
+  print("What is your name?:");
+  String? name=stdin.readLineSync();
+
+  print("What is your age?: ");
+  int? age=int.parse(stdin.readLineSync()!);
+
+  print("What is your hobby:What do you enjoy doing:");
+  String? hobby=stdin.readLineSync();
+
+  print("Which school do you study at?");
+  String? school=stdin.readLineSync();
+  //printing user details
+  print("Hii $name,You are $age years old and you study at $school.You love $hobby.");
+
+
+}
